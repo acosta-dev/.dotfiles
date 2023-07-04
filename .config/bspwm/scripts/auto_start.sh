c10a00b1056ff14a962a738179854f8d949e90b7
Orelbi Acosta <orelbi088@gmail.com>
2023-07-04 06:59:59 -0400

    Updated-Configs

--- .config/bspwm/scripts/auto_start.sh
+++ .config/bspwm/scripts/auto_start.sh
@@ -2,7 +2,6 @@
 killall polybar;polybar main &
 xsetroot -cursor_name left_ptr &
 picom &
-killall && polybar example &
 nitrogen --restore
 nm-applet &
 $HOME/Apps/Telegram/Telegram -startintray -- %u &
